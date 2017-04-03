bdc1217c38979d5d96e923e28c7ca1c0268e7e06
guilleCM <guille.mca@hotmail.com>
2017-04-03 16:13:08 +0200

    implementado main

--- Kata_NumerosRomanos/conversorNumerosRomanos/src/main/java/org/foobarspam/kata_numerosRomanos/conversorNumerosRomanos/MainProgram.java
+++ Kata_NumerosRomanos/conversorNumerosRomanos/src/main/java/org/foobarspam/kata_numerosRomanos/conversorNumerosRomanos/MainProgram.java
@@ -6,7 +6,8 @@ public class MainProgram {
 	public static void main(String[] args) {
 		
 		RomanNumerals rNum;
-		String[] romNumberGroup = {"XI", "XVII", "CLIX", "MCDXXXIX", "MMXVII", "MMCDXLIX", "MDCCXLIX"};
+		String[] romNumberGroup = {"XI", "XVII", "CLIX", "MCDXXXIX", "MMXVII", "MMCDXLIX", "MDCCXLIX",
+				"MMDCXLIV"};
 		
 		for(int i = 0; i < romNumberGroup.length; i++){
 			rNum = new RomanNumerals(romNumberGroup[i]);
